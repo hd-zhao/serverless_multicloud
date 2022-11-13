442c37864803ee711eacb91cc0eb9dc67608269d
hd-zhao <haidongzhao98@gmail.com>
2022-11-13 06:22:23 +0000

    fix

--- function/thumbnail-http/alibaba_thumbnail/index.py
+++ function/thumbnail-http/alibaba_thumbnail/index.py
@@ -12,12 +12,46 @@ def resize_image(image_path, resized_path):
 
 
 
-def handler(event, context):
+def handler(environ, start_response):
+    context = environ['fc.context']
+    request_uri = environ['fc.request_uri']
+    for k, v in environ.items():
+        if k.startswith("HTTP_"):
+            # process custom request headers
+            pass
+
+    # get request_body
+    try:
+        request_body_size = int(environ.get('CONTENT_LENGTH', 0))
+    except (ValueError):
+        request_body_size = 0
     request_body = environ['wsgi.input'].read(request_body_size)
+
+    # get request_method
+    request_method = environ['REQUEST_METHOD']
+
+    # get path info
+    path_info = environ['PATH_INFO']
+
+    # get server_protocol
+    server_protocol = environ['SERVER_PROTOCOL']
+
+    # get content_type
+    try:
+        content_type = environ['CONTENT_TYPE']
+    except (KeyError):
+        content_type = " "
+
+    # get query_string
+    try:
+        query_string = environ['QUERY_STRING']
+    except (KeyError):
+        query_string = " "
+
     evt = json.loads(request_body)
     get_time = ntp.get_timestamp_ms()
     key = evt["upload_id"]
     if(get_time != 0):
         print("Benchmark object:{} timestamp:{}".format(key, get_time))
 
-    return
+    return "hello"
